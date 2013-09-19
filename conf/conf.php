8b198bbe400fdcec62cf3f44f3fb233d49f9f585
NyDi23 <mike.v1991@gmail.com>
2013-09-19 10:55:18 +0200

    ...

---
+++ conf/conf.php
@@ -0,0 +1,13 @@
+<?php
+/*******************/
+/** Configuration **/
+/*******************/
+/** Develop mode **/
+define ('DEV_MODE', true);
+
+/** Database Setting **/
+define('DB_NAME', 'letters');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_HOST', 'localhost');
+define('DB_TYPE', 'mysql'); 
